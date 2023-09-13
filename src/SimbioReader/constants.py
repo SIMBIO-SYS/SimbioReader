4cd0b23874c4b616bebcc24623e2e9b1eb473c05
Romolo Politi <romolo.politi@inaf.it>
2023-09-13 22:27:37 +0200

    Code strurated and added some data info

---
+++ src/SimbioReader/constants.py
@@ -0,0 +1,20 @@
+class MSG:
+    ERROR = "[red][ERROR][/red] "
+    CRITICAL = "[red][CRITICAL][/red] "
+    INFO = "[green][INFO][/green] "
+    DEBUG = "[blue][DEBUG][/blue] "
+    WARNING = "[yellow][WARNING][/yellow] "
+    TODO = "[yellow][TODO][/yellow] "
+
+
+class FMODE:
+    READ = 'r'
+    READ_BINARY = 'rb'
+    WRITE = 'w'
+    WRITE_BINARY = 'wb'
+    APPEND = 'a'
+
+
+VERSION = (0,1,0,'d',2)
+
+DATAMODEL='1.15.0.0'
--- src/test.py
+++ src/test.py
@@ -4,5 +4,6 @@ from rich.console import Console
 console=Console()
 
 dat = SimbioReader(
-    "/Users/romolo.politi/Documents/Progetti/SIMBIO-SYS/Software/SimGen/output/raw/vihi/sim_raw_sc_vihi_cruise_ico4_2020-12-14_001/sim_raw_sc_vihi_internal_cruise_ico4_2020-12-14_001.dat", debug=True, verbose=True)
+    "/Users/romolo.politi/Documents/Progetti/SIMBIO-SYS/Software/SimGen/output/raw/vihi/sim_raw_sc_vihi_cruise_ico4_2020-12-14_001/sim_raw_sc_vihi_internal_cruise_ico4_2020-12-14_001.dat", debug=True, verbose=True,console=console)
+dat.Show()
 console.print(dat)
