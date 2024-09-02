cd060224cc1fe7aed175f24c5872f6a1ef21d998
Romolo Politi <romolo.politi@inaf.it>
2024-09-03 01:12:57 +0200

    fixbug readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"#"sphinx_rtd_theme" #'furo'
+html_theme = "sphinx_rtd_theme" #'furo'
 html_static_path = ['_static']
 
 
